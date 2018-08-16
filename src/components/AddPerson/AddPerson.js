37a0917b95b2baeb32a65053f70cee8251dec34a
Jet Balagtas <jetbalagtas@gmail.com>
2018-08-16 16:38:05 -0400

    connect the store to react

--- src/components/AddPerson/AddPerson.js
+++ src/components/AddPerson/AddPerson.js
@@ -3,9 +3,9 @@ import React from 'react';
 import './AddPerson.css';
 
 const addPerson = (props) => (
-    <div className="AddPerson">
-        <button onClick={props.personAdded}>Add Person</button>
-    </div>
+  <div className="AddPerson">
+    <button onClick={props.personAdded}>Add Person</button>
+  </div>
 );
 
 export default addPerson;
--- src/containers/Persons.js
+++ src/containers/Persons.js
@@ -1,44 +1,51 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Person from '../components/Person/Person';
 import AddPerson from '../components/AddPerson/AddPerson';
 
 class Persons extends Component {
-    state = {
-        persons: []
-    }
+  state = {
+    persons: []
+  }
 
-    personAddedHandler = () => {
-        const newPerson = {
-            id: Math.random(), // not really unique but good enough here!
-            name: 'Jet',
-            age: Math.floor( Math.random() * 40 )
-        }
-        this.setState( ( prevState ) => {
-            return { persons: prevState.persons.concat(newPerson)}
-        } );
+  personAddedHandler = () => {
+    const newPerson = {
+      id: Math.random(), // not really unique but good enough here!
+      name: 'Jet',
+      age: Math.floor( Math.random() * 40 )
     }
+    this.setState( ( prevState ) => {
+      return { persons: prevState.persons.concat(newPerson)}
+    });
+  }
 
-    personDeletedHandler = (personId) => {
-        this.setState( ( prevState ) => {
-            return { persons: prevState.persons.filter(person => person.id !== personId)}
-        } );
-    }
+  personDeletedHandler = (personId) => {
+    this.setState( ( prevState ) => {
+      return { persons: prevState.persons.filter(person => person.id !== personId)}
+    });
+  }
 
-    render () {
-        return (
-            <div>
-                <AddPerson personAdded={this.personAddedHandler} />
-                {this.state.persons.map(person => (
-                    <Person 
-                        key={person.id}
-                        name={person.name} 
-                        age={person.age} 
-                        clicked={() => this.personDeletedHandler(person.id)}/>
-                ))}
-            </div>
-        );
-    }
+  render () {
+    return (
+      <div>
+        <AddPerson personAdded={this.personAddedHandler} />
+        {this.state.persons.map(person => (
+          <Person 
+            key={person.id}
+            name={person.name} 
+            age={person.age} 
+            clicked={() => this.personDeletedHandler(person.id)}/>
+        ))}
+      </div>
+    );
+  }
 }
 
-export default Persons;
+const mapStateToProps = state => {
+  return {
+    ppl: state.persons
+  }
+}
+
+export default connect(mapStateToProps)(Persons);
