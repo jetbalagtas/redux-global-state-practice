e401408e81a927826f8da6371fd99b8e84b9d1ab
Jet Balagtas <jetbalagtas@gmail.com>
2018-08-16 17:47:51 -0400

    dispatch actions from component via reducer, use global state and type constants

--- src/containers/Persons.js
+++ src/containers/Persons.js
@@ -3,39 +3,19 @@ import { connect } from 'react-redux';
 
 import Person from '../components/Person/Person';
 import AddPerson from '../components/AddPerson/AddPerson';
+import * as actionTypes from '../store/actions';
 
 class Persons extends Component {
-  state = {
-    persons: []
-  }
-
-  personAddedHandler = () => {
-    const newPerson = {
-      id: Math.random(), // not really unique but good enough here!
-      name: 'Jet',
-      age: Math.floor( Math.random() * 40 )
-    }
-    this.setState( ( prevState ) => {
-      return { persons: prevState.persons.concat(newPerson)}
-    });
-  }
-
-  personDeletedHandler = (personId) => {
-    this.setState( ( prevState ) => {
-      return { persons: prevState.persons.filter(person => person.id !== personId)}
-    });
-  }
-
   render () {
     return (
       <div>
-        <AddPerson personAdded={this.personAddedHandler} />
-        {this.state.persons.map(person => (
+        <AddPerson personAdded={this.props.onPersonAdded} />
+        {this.props.ppl.map(person => (
           <Person 
             key={person.id}
             name={person.name} 
             age={person.age} 
-            clicked={() => this.personDeletedHandler(person.id)}/>
+            clicked={() => this.props.onPersonDeleted(person.id)}/>
         ))}
       </div>
     );
@@ -45,7 +25,14 @@ class Persons extends Component {
 const mapStateToProps = state => {
   return {
     ppl: state.persons
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps)(Persons);
+const mapDispatchToProps = dispatch => {
+  return {
+    onPersonAdded: () => dispatch({type: actionTypes.ADD_PERSON}),
+    onPersonDeleted: (id) => dispatch({type: actionTypes.REMOVE_PERSON, personId: id})
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Persons);
